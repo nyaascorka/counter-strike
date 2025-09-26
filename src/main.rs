72fb4da5cbf2dcb48a7de4dc8b07d4b3d124c5da
nyaascorka <nyaascorka@gmail.com>
2025-09-26 16:17:36 +0000

    init

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,26 @@ use actix_web::{HttpResponse, Responder};
 
 #[actix_web::get("/{path:.*}")]
 async fn read_page() -> impl Responder {
-    HttpResponse::Ok().body("
+    return HttpResponse::Ok().body("
         <html>
             <head>
                 <title>Path</title>
                 <meta charset='utf-8'>
             </head>
             <body>
-                Test
+                Hello everybody, I am Iscra! Call me Iscra-san or Iscra-chan. :)
             </body>
         </html>
-    ")
+    ");
 }
 
 #[actix_web::main]
 async fn main() {
     use actix_web::{App, HttpServer};
-    HttpServer::new(|| {
+    return HttpServer::new(|| {
         App::new()
             .service(read_page)
     })
         .bind("127.0.0.1:8080").unwrap()
-        .run().await.unwrap()
+        .run().await.unwrap();
 }
